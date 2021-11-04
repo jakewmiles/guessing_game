7a96616c1428336f3a60b6869ec804579762d399
jakewmiles <jake.w.miles@gmail.com>
2021-11-04 20:41:32 +0000

    feat: add colours to guess responses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use colored::*;
 
 fn main() {
     println!("Guess the number!");
@@ -24,10 +25,10 @@ fn main() {
         println!("You guessed: {}", guess);
         
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("{}", "Too small".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
             Ordering::Equal => {
-                println!("You win!");
+                println!("{}", "You win!".green());
                 break
             }
         }
